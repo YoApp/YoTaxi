d86f547fc62dc115c0203399b5a7545563c1a436
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-10-23 15:38:55 +0500

    Heroku ports setup

--- runserver.py
+++ runserver.py
@@ -1,4 +1,8 @@
 from yotaxi import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    # Cache template for fast loading
+    app.jinja_env.cache = {}
+    app.run(host='0.0.0.0', port=port, debug=True)
